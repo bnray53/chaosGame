68bbc0e2cc420f1a582eaf187fb7df74518d7106
bnray53 <bnray53@gmail.com>
2018-07-14 13:16:12 -0500

    Add logic for currentPoint

--- sketch.js
+++ sketch.js
@@ -4,16 +4,10 @@ var pointTracker = 0;
 //Number of fixed point, eventually have this be set by user
 var numPoints = 3;
 
+//flag for only allowing one starting point to be placed
 var initialPointFlag=true;
 
-//Array of five variables name to be used as reference variables for fixed point objects
-/*var pointArray = [
-  "pointOne",
-  "pointTwo",
-  "pointThree",
-  "pointFour",
-  "pointFive"
-];*/
+//Array to be used for reference variables for fixed point objects
 var pointArray=[];
 
 function setup() {
@@ -30,29 +24,40 @@ function CreatePoint(posX, posY) {
   this.posY = posY;
 }
 
+function CreateInitialPoint(posX, posY){
+	console.log("inside create initial point method");
+	point(posX, posY);
+	//set currentpt object's x and y
+	currentPoint.x=posX;
+	currentPoint.y=posY;
+	return;
+}
+
+var currentPoint={
+	x: 0,
+	y: 0
+}
+
 function mousePressed() {
   if (mouseX < width && mouseY < height) {
     if (pointTracker <= numPoints - 1) {
 	 var i=pointTracker;
-	 //pointArray[pointTracker]= "point"+pointTracker;
-	 //var arrayElement = pointArray[pointTracker-1];
-	 //var arrayElement = new CreatePoint(mouseX, mouseY);
 	 pointArray.push(new CreatePoint(mouseX, mouseY));
 	 stroke(255);
 	 ellipse(pointArray[i].posX, pointArray[i].posY, 15, 15);
 	 fill(255);
 	 console.log(pointArray[i]);
 	 pointTracker++;
-	 //console.log(arrayElement);
 	 
     } else if(pointTracker>=numPoints) {
-	 //initial point logic
-
 	 if(initialPointFlag){
-		 console.log("Pretending to call initial point method");
+		 //initial point logic
+		 //console.log("Pretending to call initial point method");
+		 CreateInitialPoint(mouseX, mouseY);
 		 initialPointFlag=false;
 	 }
 	 console.log("Too many fixed points");
+	 console.log(initialPointFlag);
 	 console.log(pointArray);
     }
   } else {
@@ -63,16 +68,15 @@ function mousePressed() {
 }
 
 function myFunction() {
-  if (pointTracker > numPoints - 1) {
+  if ((pointTracker > numPoints - 1)&& !(initialPointFlag)) {
     for (i = 0; i < 5; i++) {
 	 //Put halving logic in here
 	 //Time out with seperate function to generate points for cool factor?
-	 var x = (pointArray[0].posX + pointArray[1].posX) / 2;
-	 var y = (pointArray[0].posY + pointArray[1].posY) / 2;
+	 var x = (pointArray[0].posX + currentPoint.x) / 2;
+	 var y = (pointArray[0].posY + currentPoint.y) / 2;
 	 point(x, y);
-	 /* var x = random(0, 920);
-  	var y = random(0, 500);
-  	point(x, y);*/
+	 currentPoint.x=x;
+	 currentPoint.y=y;
     }
   } else {
     console.log("Select initial fixed points first");
