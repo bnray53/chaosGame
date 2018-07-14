e6e8f5a13a1cb80aec0810c7a29cc7acdf9ed737
bnray53 <bnray53@gmail.com>
2018-07-13 20:26:52 -0500

    Fix Formatting

--- sketch.js
+++ sketch.js
@@ -1,52 +1,55 @@
 //Tracks number of user selected fixed points
-var pointTracker=0;
+var pointTracker = 0;
 
 //Array of five variables name to be used as reference variables for fixed point objects
-var pointArray=["pointOne","pointTwo","pointThree","pointFour","pointFive"];
+var pointArray = [
+  "pointOne",
+  "pointTwo",
+  "pointThree",
+  "pointFour",
+  "pointFive"
+];
 
 function setup() {
   // put setup code here
   createCanvas(920, 500);
-  background(100);  
+  background(100);
 }
 
-function draw(){
-
-}
+function draw() {}
 
 //Constructor for fixed points
-function CreatePoint(posX, posY){
-  this.posX=posX;
-  this.posY=posY;
+function CreatePoint(posX, posY) {
+  this.posX = posX;
+  this.posY = posY;
 }
 
-function mousePressed(){
-  if(mouseX<width && mouseY<height){ 
-    if(pointTracker<=4){
-      var arrayElement=pointArray[pointTracker];
-      var arrayElement=new CreatePoint(mouseX, mouseY);
+function mousePressed() {
+  if (mouseX < width && mouseY < height) {
+    if (pointTracker <= 4) {
+      var arrayElement = pointArray[pointTracker];
+      var arrayElement = new CreatePoint(mouseX, mouseY);
       stroke(255);
       console.log("Inside mousePressed");
       ellipse(arrayElement.posX, arrayElement.posY, 15, 15);
       fill(255);
       pointTracker++;
       //point(100, 100);
-    }else{
+    } else {
       console.log("Too many ellipses");
     }
-  }  
-}
-  
-  function myFunction(){
-     if(pointTracker>4){
-      for(i=0; i<1000; i++){
-        console.log("Activated");
-        var x=random(0, 920);
-        var y=random(0, 500);
-        point(x,y);
-      }
-    }else{
-      console.log("select initial fixed point first");
-    }   
   }
- 
+}
+
+function myFunction() {
+  if (pointTracker > 4) {
+    for (i = 0; i < 1000; i++) {
+      console.log("Activated");
+      var x = random(0, 920);
+      var y = random(0, 500);
+      point(x, y);
+    }
+  } else {
+    console.log("select initial fixed point first");
+  }
+}
