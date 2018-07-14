3a11ae9793461ebf1da48fb8a5a0087d06bc344a
bnray53 <bnray53@gmail.com>
2018-07-13 23:18:45 -0500

    Add single variable for number of points

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 //Tracks number of user selected fixed points
 var pointTracker = 0;
 
+//Number of fixed point, eventually have this be set by user
+var numPoints=3;
+
 //Array of five variables name to be used as reference variables for fixed point objects
 var pointArray = [
   "pointOne",
@@ -26,30 +29,33 @@ function CreatePoint(posX, posY) {
 
 function mousePressed() {
   if (mouseX < width && mouseY < height) {
-    if (pointTracker <= 4) {
+    if (pointTracker <= (numPoints-1)) {
       var arrayElement = pointArray[pointTracker];
       var arrayElement = new CreatePoint(mouseX, mouseY);
       stroke(255);
-      console.log("Inside mousePressed");
       ellipse(arrayElement.posX, arrayElement.posY, 15, 15);
       fill(255);
       pointTracker++;
-      //point(100, 100);
     } else {
-      console.log("Too many ellipses");
+      console.log("Too many fixed points");
     }
+  }else{
+    if(pointTracker<=(numPoints-1)){
+      console.log("Please click inside the canvas area");
+    }  
   }
 }
 
 function myFunction() {
-  if (pointTracker > 4) {
-    for (i = 0; i < 1000; i++) {
-      console.log("Activated");
+  if (pointTracker > (numPoints-1)) {
+    for (i = 0; i < 10000; i++) {
+      //Put halving logic in here
+      //Time out with seperate function to generate points for cool factor?
       var x = random(0, 920);
       var y = random(0, 500);
       point(x, y);
     }
   } else {
-    console.log("select initial fixed point first");
+    console.log("Pelect initial fixed points first");
   }
 }
