fb1cf59ad83ee1900036b9adb7de24979704404f
bnray53 <bnray53@gmail.com>
2018-07-13 20:25:43 -0500

    Initial Logic

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
-var x=0;
-var ellipseTracker=0;
+//Tracks number of user selected fixed points
+var pointTracker=0;
+
+//Array of five variables name to be used as reference variables for fixed point objects
+var pointArray=["pointOne","pointTwo","pointThree","pointFour","pointFive"];
 
 function setup() {
   // put setup code here
@@ -7,23 +10,26 @@ function setup() {
   background(100);  
 }
 
-function draw() {
+function draw(){
 
+}
 
+//Constructor for fixed points
+function CreatePoint(posX, posY){
+  this.posX=posX;
+  this.posY=posY;
 }
 
 function mousePressed(){
   if(mouseX<width && mouseY<height){ 
-    if(ellipseTracker<5){
+    if(pointTracker<=4){
+      var arrayElement=pointArray[pointTracker];
+      var arrayElement=new CreatePoint(mouseX, mouseY);
       stroke(255);
-      ellipse(mouseX, mouseY, 15, 15);
-      x=x+1;
-      console.log(x);
-      console.log(mouseX);
-      console.log(mouseY);
-      console.log("__________________________________");
+      console.log("Inside mousePressed");
+      ellipse(arrayElement.posX, arrayElement.posY, 15, 15);
       fill(255);
-      ellipseTracker++;
+      pointTracker++;
       //point(100, 100);
     }else{
       console.log("Too many ellipses");
@@ -32,9 +38,15 @@ function mousePressed(){
 }
   
   function myFunction(){
-    console.log("Activated");
-    var x=random(0, 920);
-    var y=random(0, 500);
-    point(x,y);
+     if(pointTracker>4){
+      for(i=0; i<1000; i++){
+        console.log("Activated");
+        var x=random(0, 920);
+        var y=random(0, 500);
+        point(x,y);
+      }
+    }else{
+      console.log("select initial fixed point first");
+    }   
   }
  
